d32c806d9b02eb6addf0014378a04462df37008d
ShashiniU <shashiuwanshi97@gmail.com>
2022-11-11 18:24:06 +0000

    2nd

--- HeartDiseaseprecdictin.py
+++ HeartDiseaseprecdictin.py
@@ -1,3 +1,16 @@
-print("Hellow world")
- git config --global user.email "shashiuwanshi97@gmail.com"
-  git config --global user.name "ShashiniU"
+import numpy as np
+import pandas as pandas
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+#load cvs data to pandas dataframe
+heartData = pandas.read_csv('heart.csv')python
+#first 5 rows
+heartData.head()
